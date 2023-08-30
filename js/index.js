8a79a8bc427278447761c67c9c342e17202bebf6
Florencia Ayala <flor.ayala30@gmail.com>
2023-08-30 19:46:38 -0300

    se agregaron nuevos archivos js y funcionalidad en los eventos

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 console.log(data);
+const $div = document.querySelector('#eventos')
+
+console.log($div); 
+ 
+let  template  = ''
+for ( let evento of data.events){
+    template  += ` <div>
+    <div class="card " id="styleCards">
+        <img src="${evento.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${evento.name}</h5>
+            <p class="card-text">${evento.description}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    </div>`
+}
+
+console.log( template )
+$div.innerHTML =  template 
--- js/upcoming-events.js
+++ js/upcoming-events.js
@@ -1,1 +1,42 @@
 console.log(data);
+// console.log(data);
+
+let  template  = ''
+let pastEvents = []
+const currentDate = data.currentDate
+
+
+const $div = document.querySelector('#eventos')
+
+// console.log($div);
+
+
+function filtroFecha(date){
+    for(let evento of data.events){
+        if (date <= evento.date){
+            pastEvents.push(evento)
+        }
+    }
+
+}
+filtroFecha(currentDate)
+console.log(pastEvents)
+
+for ( let evento of pastEvents){
+   
+        template  += ` <div>
+        <div class="card " id="styleCards">
+            <img src="${evento.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="${evento.name}">${evento.category}</h5>
+                <p class="card1-text">${evento.description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>`
+    
+
+}
+
+console.log( template )
+$div.innerHTML =  template
